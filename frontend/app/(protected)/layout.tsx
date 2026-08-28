dcdea8118444b2c6ed235b43e767869e4f66ff6a
JessicaHo-CMK <jessica.ho@ext.centralmegakencana.com>
2026-08-28 15:17:05 +0700

    Create Layout
    Create layout with sidebar, header, and footer; change font on global layout

---
+++ frontend/app/(protected)/layout.tsx
@@ -0,0 +1,25 @@
+"use client"
+
+import { ReactNode } from "react"
+
+import { Header } from "@/components/layout/Header/Header";
+import { Footer } from "@/components/layout/Footer/Footer";
+import { AppSidebar } from "@/components/layout/Sidebar/app-sidebar";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+
+export default function ProtectedLayout({ children }: { children: ReactNode; }) {
+    return (
+        <SidebarProvider className="flex flex-col min-h-full">
+            <Header>
+                <SidebarTrigger />
+            </Header>
+            <div className="flex flex-1 w-full">
+                <AppSidebar />
+                <main className="flex-1 mt-2.5 mx-5 mb-0 min-h-[86vh]">
+                    {children}
+                </main>
+            </div>
+            <Footer />
+        </SidebarProvider >
+    );
+}
