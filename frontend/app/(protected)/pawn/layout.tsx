c84785e1b5a53bd5469469165d46dfe3e0f62a7c
JessicaHo-CMK <jessica.ho@ext.centralmegakencana.com>
2026-09-02 09:41:48 +0700

    Update pawn & application layout

--- frontend/app/(protected)/pawn/layout.tsx
+++ frontend/app/(protected)/pawn/layout.tsx
@@ -1,15 +1,6 @@
 "use client"
 
-import {
-    HandCoins,
-} from "lucide-react";
-
-// Isi Default Breadcrumb
-export const defaultBreadcrumb = [
-  { label: "Pawn", href: "/pawn", icon: HandCoins },
-]
-
-export default function ApplicationLayout({ children }: { children: React.ReactNode }) {
+export default function PawnLayout({ children }: { children: React.ReactNode }) {
   return (
     <div> {children} </div>
     )
