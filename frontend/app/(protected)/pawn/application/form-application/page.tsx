ee13f4df27d0df7efde0e23484c0eaf8529902f6
JessicaHo-CMK <jessica.ho@ext.centralmegakencana.com>
2026-09-02 10:46:02 +0700

    Add application form page and update dialog tsx

---
+++ frontend/app/(protected)/pawn/application/form-application/page.tsx
@@ -0,0 +1,27 @@
+"use client"
+
+// Style card
+import { style_card } from "@/components/shared/Stepper/Stepper";
+
+// Component
+// import { BarangTable } from "./_components/barang-table";
+// import { ModalLayout } from "./_components/modal-layout";
+
+// Data
+// import { dataBarang } from "./_data/barang-data";
+
+export default function FormApplication() {
+  return (
+    <div className={`${style_card} w-full`}>
+      {/* Judul & Button*/}
+      <div className="md:flex justify-between align-middle">
+        <h1 className="font-bold pb-2">Daftar Barang</h1>
+
+        {/* Form */}
+        {/* <ModalLayout /> */}
+      </div>
+      {/* Tabel */}
+      {/* <BarangTable data={dataBarang} /> */}
+    </div>
+  )
+}
